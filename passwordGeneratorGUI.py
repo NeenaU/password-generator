4a5f10d87dbf6a9ebfc9bdd718c600dc5224b94b
Neena Ugail <nhu21@bath.ac.uk>
2020-07-27 19:05:23 +0100

    added copy to clipboard feature

--- passwordGeneratorGUI.py
+++ passwordGeneratorGUI.py
@@ -1,5 +1,6 @@
 import random
 import tkinter as tk
+import pyperclip
 
 class passwordGenerator():
 
@@ -12,41 +13,43 @@ class passwordGenerator():
         self.uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
         self.numbersList = ["0","1","2","3","4","5","6","7","8","9"]
         self.symbolsList = ["!","\"","£","$","%","^","&","*","(",")","_","-","+","=","[","{","]","}",";",":","@","\'","#","<",">","?","/"]
-
-        #Frames
-        #entryFrame = Frame(master, bg='aaabb8')
-
-        self.titleLabel = tk.Label(text="Password Generator", font=("Times",16)).grid(sticky='NW',columnspan=10,pady=15)
-        self.charactersLabel = tk.Label(text="Number of characters:").grid(sticky='W')
-        self.characterEntry = tk.Entry(width=3)
-        self.characterEntry.grid(row=1,column=1,pady=10,sticky='W')
-
-        self.generateButton = tk.Button(text="Generate Password", command=self.verifyCheckboxes, bg='#464866').grid(sticky='W',pady=10)
-        self.resultLabel = tk.Label(text="Result:", font=("Times",10))
-        self.resultLabel.grid(sticky='W')
-        self.resetButton = tk.Button(text="Reset Options", command=self.reset).grid(row=4,column=0,pady=10,sticky='W')
-
-        self.errorLabel = tk.Label(text="")
-        self.errorLabel.grid(sticky='W')
-
-        self.optionsLabel = tk.Label(text="Character Options", font=("Times", 11)).grid(sticky='W')
         self.acceptedCharacters = []
+
+        #Title
+        self.titleLabel = tk.Label(text="Password Generator", font=("Times",16), bg='#aaabb8').grid(sticky='NW',columnspan=11,pady=15)
+
+        #Widgets for the options
+        
+        self.optionsLabel = tk.Label(text="Character Options", font=("Times", 11), bg='#aaabb8').grid(sticky='W')
+        self.charactersLabel = tk.Label(master, text="Number of characters:", bg='#aaabb8').grid(sticky='W')
+        self.characterEntry = tk.Entry(master, width=3)
+        self.characterEntry.grid(row=2,column=1,pady=10,sticky='W')
         
         self.lowercase = tk.IntVar()
         self.uppercase = tk.IntVar()
         self.numbers = tk.IntVar()
         self.symbols = tk.IntVar()
+        self.lowercaseCheckbutton = tk.Checkbutton(master, text="Lowercase letters", variable=self.lowercase, bg='#aaabb8').grid(sticky='W')
+        self.uppercaseCheckbutton = tk.Checkbutton(master, text="Uppercase letters", variable=self.uppercase, bg='#aaabb8').grid(sticky='W')
+        self.numbersCheckbutton = tk.Checkbutton(master, text="Numbers", variable=self.numbers, bg='#aaabb8').grid(sticky='W')
+        self.symbolsCheckbutton = tk.Checkbutton(master, text="Symbols", variable=self.symbols, bg='#aaabb8').grid(sticky='W')
 
-        self.lowercaseCheckbutton = tk.Checkbutton(master, text="Lowercase letters", variable=self.lowercase).grid(sticky='W')
-        self.uppercaseCheckbutton = tk.Checkbutton(master, text="Uppercase letters", variable=self.uppercase).grid(sticky='W')
-        self.numbersCheckbutton = tk.Checkbutton(master, text="Numbers", variable=self.numbers).grid(sticky='W')
-        self.symbolsCheckbutton = tk.Checkbutton(master, text="Symbols", variable=self.symbols).grid(sticky='W')
+        self.resetButton = tk.Button(text="Reset Options", command=self.reset).grid(row=7,column=0,sticky='W',pady=10)
 
+        #Widgets for the result
+        self.resultLabel = tk.Label(text="", font=("Times",10), bg='#aaabb8')
+        self.resultLabel.grid(row=3,column=12,sticky='W',padx=10)
+        self.generateButton = tk.Button(text="Generate Password", command=self.verify).grid(row=5,column=12,sticky='W',padx=10)
         
+        #Widget for displaying errors
+        self.errorLabel = tk.Label(text="", bg='#aaabb8')
+        self.errorLabel.grid(row=6,column=12,sticky='W',padx=10)
 
-    def verifyCheckboxes(self):
 
-        self.errorLabel["text"] = ""    #reset error label
+    #Error handling of checkboxes and entry box for number of characters
+    def verify(self):
+
+        self.errorLabel["text"] = ""    #reset error label and list of accepted characters
         self.acceptedCharacters = []
         
         if len(self.characterEntry.get()) > 0:
@@ -55,6 +58,8 @@ class passwordGenerator():
                 if int(self.characterEntry.get()) >= 0:
                     if self.lowercase.get() == 1 or self.uppercase.get() == 1 or self.numbers.get() == 1 or self.symbols.get() == 1:
 
+
+                        #Numbers correspond to a type of character (1=lowercase, 2=uppercase, 3=number, 4=symbol)
                         if self.lowercase.get() == 1:
                             self.acceptedCharacters.append(1)
                             
@@ -75,13 +80,15 @@ class passwordGenerator():
                 else:
                     self.errorLabel["text"] = "Choose a number that is greater than zero"
                 
-            except:
+            except Exception as e:
                 self.errorLabel["text"] = "Please enter a number"
+                print(e)
                 
 
         else:
             self.errorLabel["text"] = "Enter a number in the box"
 
+    #For each character: choose a random type of character, then a random character from the corresponding list
     def generatePassword(self):
         charNum = int(self.characterEntry.get())
         password = []
@@ -103,12 +110,18 @@ class passwordGenerator():
                 password.append(random.choice(self.symbolsList))
 
         password = "".join(password)
-        password = "Result: " + password
-        self.resultLabel["text"] = password
-     
+        passwordWithText = "Result: " + password
+        self.resultLabel["text"] = passwordWithText
+
+        #Copy password to clipboard
+        pyperclip.copy(password)
+        self.errorLabel["text"] = "Copied to clipboard!"
+
+    #Reset everything to be empty/default values
     def reset(self):
+        
         self.acceptedCharacters = []
-        self.resultLabel["text"] = "Result:"
+        self.resultLabel["text"] = ""
         self.errorLabel["text"] = ""
         
         if len(self.characterEntry.get()) > 0:
@@ -121,8 +134,8 @@ class passwordGenerator():
 
 def main():
     window = tk.Tk()
-    window.geometry("350x250")
-    window.configure(bg='#aaabb8')
+    window.geometry("400x275")
+    window.configure(bg='#aaabb8')  #window background colour
     generator = passwordGenerator(window)
     window.mainloop()
 
